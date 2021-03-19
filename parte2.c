1e8354e5ec916d7983ce99ea808af3fc00a3828d
Mario Ortega <marioal9806@hotmail.com>
2021-03-19 17:36:38 -0600

    Lectura de datos cada 10 seg

--- parte2.c
+++ parte2.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <unistd.h>
 
 #define MODE_READ 0
 #define MODE_WRITE 1
@@ -23,9 +24,18 @@ uint8_t slave_address = 0x00;
 uint32_t len = 0;
 uint8_t  mode = MODE_READ;
 
+uint8_t  initial = I2C_BEGIN;
+uint16_t clk=2500;
+uint8_t slave;
+uint32_t len;
+uint8_t  mode2=MODE_WRITE;
+
 //comparse
 
 char buf[MAX_LEN];
+char buf2[MAX_LEN];
+char buf3[MAX_LEN];
+char buf4[MAX_LEN];
 int i;
 uint8_t data;
 FILE *f;
@@ -65,24 +75,7 @@ int createpack (uint8_t  init, uint16_t clk_div, uint8_t slave_address,
 			 //printf("Read Buf[%d] = %d\n", i, buf[i]);
 		} 
 	}
-	    if(slave_address == 104){
-            fprintf(f, "Record %d: ", x);
-            fprintf(f, "%x/", buf[4]); //dia
-            fprintf(f, "%x/", buf[5]); //mes
-            fprintf(f, "%x", buf[6]); //año
-            //Dia de la semana
-            char *weekdays[] = {"", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-            fprintf(f, " %s ", weekdays[buf[3]]);
-            fprintf(f, "%x", buf[2] & 31); //horas 
-            fprintf(f, ":%x:", buf[1]); //minutos
-            fprintf(f, "%x", buf[0]); //segundos
-            // PM o AM -> bit 5 de buf [2]
-            int AM_PM = ((buf[2] << 2) >> 7);
-            fprintf(f, " %s\n", AM_PM ? "PM" : "AM");
-            x++; //para el siguiente record
-	    }else{
-        	fprintf(f, "Tempreture: %d°C\n", buf[0]); 
-    	}
+	    
     }
 
     if (mode == MODE_WRITE) {
@@ -97,38 +90,93 @@ int createpack (uint8_t  init, uint16_t clk_div, uint8_t slave_address,
     return 1;
 }
 
-int main(int argc, char **argv) {
-
-    printf("Running ... \n");
-    
-    // parse the command line
-    //if (comparse(argc, argv) == EXIT_FAILURE) return showusage (EXIT_FAILURE);
-    f=fopen("log.txt", "w");
-    
-    uint8_t  initial = I2C_BEGIN;
-    uint16_t clk=2500;
-    uint8_t slave=77;
-    uint32_t len=1;
-    uint8_t  mode2=MODE_WRITE;
-
+void PacketeSensor(){
+    slave = 77;
+    len = 1;
+    mode2=MODE_WRITE;
     memset(wbuf, 0, sizeof(wbuf));
 
     createpack(initial, clk, slave, len, mode2);
 
     mode2=MODE_READ;
-
     createpack(initial, clk, slave, len, mode2);
+}
 
+void PacketeReloj(){
     slave = 104;
+    len = 1;
     mode2=MODE_WRITE;
     memset(wbuf, 0, sizeof(wbuf));
-    
     createpack(initial, clk, slave, len, mode2);
 
     mode2=MODE_READ;
     len = 7;
     createpack(initial, clk, slave, len, mode2);
+}
 
+void printTiempo(char bufa[MAX_LEN]){
+  fprintf(f, "Record %d: ", x);
+  fprintf(f, "%x/", bufa[4]); //dia
+  fprintf(f, "%x/", bufa[5]); //mes
+  fprintf(f, "%x", bufa[6]); //año
+  //Dia de la semana
+  char *weekdays[] = {"", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+  fprintf(f, " %s ", weekdays[bufa[3]]);
+  fprintf(f, "%x", bufa[2] & 31); //horas 
+  fprintf(f, ":%x:", bufa[1]); //minutos
+  fprintf(f, "%x", bufa[0]); //segundos
+  // PM o AM -> bit 5 de buf [2]
+  int AM_PM = ((bufa[2] << 2) >> 7);
+  fprintf(f, " %s\n", AM_PM ? "PM" : "AM");
+  x++; //para el siguiente record
+}
+
+void printTemp(){
+  fprintf(f, "Tempreture: %d°C\n", buf[0]); 
+}
+
+int main(int argc, char **argv) {
+
+    printf("Running ... \n");
+    int contadorS = 0;
+    int Z=0;
+    
+    while(Z<=30){
+      sleep(1);
+      if (contadorS >= 10) {
+       //PacketeSensor();
+       //printTemp();
+       PacketeReloj();
+       //printTiempo();
+       for(int d=0; d<7; d++){
+          buf4[d]=buf3[d];
+          buf3[d]=buf2[d];
+          buf2[d]=buf[d];
+       }
+       contadorS = 0;
+      }
+      contadorS ++;
+      PacketeSensor();
+      if (buf[0]>=30){
+        PacketeReloj();
+        //printTiempo();
+        for(int d=0; d<7; d++){
+          buf4[d]=buf3[d];
+          buf3[d]=buf2[d];
+          buf2[d]=buf[d];
+        }
+      }
+      Z++;
+    }
+
+    f=fopen("log.txt", "w");
+    PacketeSensor();
+    printTemp();
+
+    printTiempo(buf4);
+    printTiempo(buf3);
+    printTiempo(buf2);
+    
     fclose(f);   
     printf("... done!\n");
     return 0;
